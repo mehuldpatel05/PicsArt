e562d5a60dedc7c9facd52217503adde6dcab89d
MehulFPrimec <50493584+MehulFPrimec@users.noreply.github.com>
2022-01-07 14:43:47 +0530

    Code cleanup

--- PicsArt/ViewController.swift
+++ PicsArt/ViewController.swift
@@ -269,10 +269,3 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
 }
-
-extension UICollectionView {
-    func deselectAllItems(animated: Bool) {
-        guard let selectedItems = indexPathsForSelectedItems else { return }
-        for indexPath in selectedItems { deselectItem(at: indexPath, animated: animated) }
-    }
-}
